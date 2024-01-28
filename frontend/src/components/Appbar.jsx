767d92011532b505df1566e4de58d0d75025336a
Rahul Kumar <rahulkhg980@gmail.com>
2024-01-28 17:10:12 +0530

    first commit

--- frontend/src/components/Appbar.jsx
+++ frontend/src/components/Appbar.jsx
@@ -1,25 +1,50 @@
-import { useLocation } from 'react-router-dom';
+import React, { useState } from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
 
 export const Appbar = () => {
-    const location = useLocation();
+  const location = useLocation();
+  const navigate = useNavigate();
 
-    const { para ,name } = location.state || { para: 'U' };
-   
-    return (
-        <div className="shadow h-14 flex justify-between">
-            <div className="flex flex-col justify-center h-full ml-4">
-                PayTM App
-            </div>
-            <div className="flex">
-                <div className="flex flex-col justify-center h-full mr-4">
-                    Hello
-                </div>
-                <div className="rounded-full h-12 w-12 bg-slate-200 flex justify-center mt-1 mr-2">
-                    <div className="flex flex-col justify-center h-full text-xl">
-                        {para ? para[0].toUpperCase() : name[0]}
-                    </div>
-                </div>
-            </div>
+  const [dropdownVisible, setDropdownVisible] = useState(false);
+
+  const { para, name } = location.state || { para: 'U' };
+
+  const handleDropdownClick = () => {
+    setDropdownVisible(!dropdownVisible);
+  };
+
+  const handleLogout = () => {
+    // Add your logout logic here, such as clearing tokens or redirecting to the login page
+    localStorage.removeItem('token');
+    navigate('/signin');
+  };
+
+  return (
+    <div className="shadow h-14 flex justify-between">
+      <div className="flex flex-col justify-center h-full ml-4">
+        PayTM App
+      </div>
+      <div className="relative flex">
+        <div
+          className="flex flex-col justify-center h-full mr-4 " >
+          Hello
         </div>
-    );
+        <div className="rounded-full h-12 w-12 bg-slate-200 flex justify-center mt-1 mr-2">
+          <div  onClick={handleDropdownClick} className="flex flex-col justify-center h-full text-xl cursor-pointer">
+            {para ? para[0].toUpperCase() : name[0].toUpperCase()}
+          </div>
+        </div>
+        {dropdownVisible && (
+          <div className="absolute right-1 mt-14 bg-white border border-gray-300 rounded-md shadow-lg">
+            <div
+              className="p-2 cursor-pointer hover:bg-gray-100"
+              onClick={handleLogout}
+            >
+              Logout
+            </div>
+          </div>
+        )}
+      </div>
+    </div>
+  );
 };
